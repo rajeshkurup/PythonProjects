5c2a2b9279343fecc5541723abc3bbf2f27315f8
Rajesh Raghavakurup <rtraghavakurup@paypal.com>
2015-10-03 13:50:15 -0700

    adding html form samples

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -1,16 +1,43 @@
 import web
 
 urls = (
-	'/', 'Index'
+	'/', 'Index',
+	'/hello', 'Hello',
+	'/hello_form', 'HelloForm',
+	'/hello_form_layout', 'HelloFormLayout'
 )
 
 app = web.application(urls, globals())
 
-render = web.template.render('templates/')
+render = web.template.render('templates/', base="layout")
 
 class Index(object):
 	def GET(self):
 		greeting = "Hello World"
+		return render.index(greeting = greeting)
+		
+class Hello(object):
+	def GET(self):
+		form = web.input(name="Nobody")
+		greeting = "Hello, %s" % form.name
+		return render.index(greeting = greeting)
+
+class HelloForm(object):
+	def GET(self):
+		return render.hello_form()
+	
+	def POST(self):
+		form = web.input(name="NoBody", greet="Hello")
+		greeting = "%s, %s" % (form.greet, form.name)
+		return render.index(greeting = greeting)
+		
+class HelloFormLayout(object):
+	def GET(self):
+		return render.hello_form_layout()
+	
+	def POST(self):
+		form = web.input(name="NoBody", greet="Hello")
+		greeting = "%s, %s" % (form.greet, form.name)
 		return render.index(greeting = greeting)
 		
 if __name__ == "__main__":
