476e78db343757498ff3b636ffc3a4b2c5380a85
Rajesh Raghavakurup <rtraghavakurup@paypal.com>
2015-10-07 23:26:54 -0700

    adding mongo db samples

--- projects/rajesh_family_trust/bin/app.py
+++ projects/rajesh_family_trust/bin/app.py
@@ -1,11 +1,19 @@
 import web
 import markdown
+import json
+import pymongo
 from rajesh_family_trust.family_member import *
+from bson import json_util
+from pymongo import MongoClient
 
 urls = (
 	'/', 'Index',
 	'/family_members', 'FamilyMembers',
-	'/family_member', 'FamilyMember'
+	'/family_member', 'FamilyMember',
+	'/family_member_edit', 'FamilyMemberEdit',
+	'/family_member_submit', 'FamilyMemberSubmit',
+	'/family_member_add', 'FamilyMemberAdd',
+	'/family_member_create', 'FamilyMemberCreate'
 )
 
 app = web.application(urls, globals())
@@ -13,7 +21,7 @@ app = web.application(urls, globals())
 # little hack so that debug mode works with sessions
 if web.config.get('_session') is None:
 	store = web.session.DiskStore('sessions')
-	session = web.session.Session(app, store, initializer={'page': None})
+	session = web.session.Session(app, store, initializer={'selected_family_member': None})
 	web.config._session = session
 else:
 	session = web.config._session
@@ -22,8 +30,16 @@ render = web.template.render('templates/')
 
 md = markdown.Markdown(output_format='html4')
 
+mongo_client = MongoClient('mongodb://localhost:27017/')
+mongo_db_rft = mongo_client.rajesh_family_trust
+
 class Index(object):
 	def GET(self):
+		if mongo_db_rft.family_members.count() > 0:
+			session.selected_family_member = mongo_db_rft.family_members.find().next().get("id")
+		else:
+			session.selected_family_member = 0
+				
 		html_content = '<h1>Rajesh Family Trust</h1>'
 		html_content += '<br><p>The trust of Rajesh family members to handle all family matters</p>'
 		html_content += '<br><p><a href="/family_members">Family Members</a></p>'
@@ -34,47 +50,168 @@ class FamilyMembers(object):
 	def GET(self):
 		html_content = '<h1>Family Members</h1>'
 		html_content += '<br><p>List of family members of Rajesh Family Trust</p>'
+		
 		html_content += '<br><p><form action="/family_member" method="POST">'
-		limit = len(family_members) + 1
-
-		for position in range(1, limit):
-			str_position = str(position)
-			html_content += '<input type="radio" name="member_name" value="'
-			html_content += str_position
+	
+		if session.selected_family_member > 0:	
+			for family_member in mongo_db_rft.family_members.find():	
+				html_content += '<br><input type="radio" name="member_name" value="%d"' % family_member.get("id")
 			
-			if position == 1:
-				html_content += '" checked="true"/> '
-			else:
-				html_content += '"/> '
+				if session.selected_family_member == family_member.get("id"):
+					html_content += ' checked /> %s' % family_member.get("name")
+				else:
+					html_content += ' /> %s' % family_member.get("name")
 
-			html_content += family_members[str_position].name
-			html_content += '<br>'
-			position += 1
-
-		html_content += '<input type="submit" name="view_details" value="View Details"/>'
-		html_content += '</form></p>'
+			html_content += '<br><br><input type="submit" name="btn_input" value="Home" />'
+			html_content += '<input type="submit" name="btn_input" value="View" />'
+			html_content += '<input type="submit" name="btn_input" value="Edit" />'
+			html_content += '<input type="submit" name="btn_input" value="Delete" />'
+		
+		html_content += '<input type="submit" name="btn_input" value="Add" /></form></p>'
 		content = md.convert(html_content)
 		return render.layout(content=content)
 		
 class FamilyMember(object):
-	def POST(self):
-		form = web.input(member_name=[])
-		family_member = family_members[form.member_name[0]]
+	def GET(self):
+		family_member = mongo_db_rft.family_members.find({"id" : session.selected_family_member}).next()
 		html_content = '<h1>Family Member</h1>'
 		html_content += '<br><p>Details of Family Member</p>'
-		html_content += '<br><p>Name: '
-		html_content += family_member.name
-		html_content += '</p><br><p>Age: '
-		html_content += family_member.age
-		html_content += '</p><br><p>Gender: '
-		html_content += family_member.gender
-		html_content += '</p><br><p>Job: '
-		html_content += family_member.job
-		html_content += '</p><br><p>Office: '
-		html_content += family_member.office
-		html_content += '</p>'
+		html_content += '<br><p><form action="/family_member_edit" method="POST">'
+		html_content += '<br><p>Name: %s </p>' % family_member.get("name")
+		html_content += '<br><p>Age: %s </p>' % family_member.get("age")
+		html_content += '<br><p>Gender: %s </p>' % family_member.get("gender")
+		html_content += '<br><p>Job: %s </p>' % family_member.get("job")
+		html_content += '<br><p>Office: %s </p>' % family_member.get("office")
+		html_content += '<br><input type="submit" name="btn_edit" value="Edit" />'
+		html_content += '<input type="submit" name="btn_family_members" value="Family Members" />'
+		html_content += '<input type="submit" name="btn_home" value="Home" /></form></p>'
 		content = md.convert(html_content)
 		return render.layout(content=content)
 
+	def POST(self):
+		form = web.input(btn_input=None, member_name=None)
+		
+		if form.member_name:
+			session.selected_family_member = int(form.member_name)
+
+		if form.btn_input == "View":
+			web.seeother("/family_member")
+
+		elif form.btn_input == "Edit":
+			web.seeother("/family_member_edit")
+
+		elif form.btn_input == "Add":
+			web.seeother("/family_member_add")
+
+		elif form.btn_input == "Delete":
+			mongo_db_rft.family_members.remove({"id" : session.selected_family_member})
+			
+			if mongo_db_rft.family_members.count() > 0:
+				session.selected_family_member = mongo_db_rft.family_members.find().next().get("id")
+			else:
+				session.selected_family_member = 0
+	
+			web.seeother("/family_members")
+
+		elif form.btn_input == "Home":
+			web.seeother("/")
+			
+		else:
+			web.seeother("/")
+		
+class FamilyMemberEdit(object):
+	def GET(self):
+		family_member = mongo_db_rft.family_members.find({"id" : session.selected_family_member}).next()
+		html_content = '<h1>Edit Family Member</h1>'
+		html_content += '<br><p>Edit the Details of Family Member</p>'
+		html_content += '<br><p><form action="/family_member_submit" method="POST">'
+		html_content += '<br><p>Name: <input type="text" name="name" value="%s" /></p>' % family_member.get("name")
+		html_content += '<br><p>Age: <input type="text" name="age" value="%s" /></p>' % family_member.get("age")
+		html_content += '<br><p>Gender: <input type="text" name="gender" value="%s" /></p>' % family_member.get("gender")
+		html_content += '<br><p>Job: <input type="text" name="job" value="%s" /></p>' % family_member.get("job")
+		html_content += '<br><p>Office: <input type="text" name="office" value="%s" /></p>' % family_member.get("office")
+		html_content += '<br><input type="submit" name="btn_submit" value="Submit" />'
+		html_content += '<input type="submit" name="btn_reset" value="Reset" />'
+		html_content += '<input type="submit" name="btn_cancel" value="Cancel" /></form></p>'
+		content = md.convert(html_content)
+		return render.layout(content=content)
+	
+	def POST(self):
+		form = web.input(btn_edit=None, btn_family_members=None, btn_home=None)
+		if form.btn_edit:
+			web.seeother("/family_member_edit")
+			
+		elif form.btn_family_members:
+			web.seeother("/family_members")
+			
+		elif form.btn_home:
+			web.seeother("/")
+			
+		else:
+			web.seeother("/")
+		
+class FamilyMemberSubmit(object):
+	def POST(self):
+		form = web.input(btn_submit=None, btn_reset=None, btn_cancel=None, name=None, age=None, gender=None, job=None, office=None)
+		if form.btn_submit:
+			mongo_db_rft.family_members.update({"id" : session.selected_family_member}, 
+												{"id" : session.selected_family_member, 
+												"name" : form.name,
+												"age" : form.age,
+												"gender" : form.gender,
+												"job" : form.job,
+												"office" : form.office})
+
+			web.seeother("/family_member")
+			
+		elif form.btn_reset:
+			web.seeother("/family_member_edit")
+		
+		elif form.btn_cancel:
+			web.seeother("/family_member")
+		
+		else:
+			web.seeother("/")
+
+class FamilyMemberAdd(object):
+	def GET(self):
+		html_content = '<h1>Add Family Member</h1>'
+		html_content += '<br><p>Add new Family Member</p>'
+		html_content += '<br><p><form action="/family_member_create" method="POST">'
+		html_content += '<br><p>Name: <input type="text" name="name" value="" /></p>'
+		html_content += '<br><p>Age: <input type="text" name="age" value="" /></p>'
+		html_content += '<br><p>Gender: <input type="text" name="gender" value="" /></p>'
+		html_content += '<br><p>Job: <input type="text" name="job" value="" /></p>'
+		html_content += '<br><p>Office: <input type="text" name="office" value="" /></p>'
+		html_content += '<br><input type="submit" name="btn_add" value="Add" />'
+		html_content += '<input type="submit" name="btn_reset" value="Reset" />'
+		html_content += '<input type="submit" name="btn_cancel" value="Cancel" /></form></p>'
+		content = md.convert(html_content)
+		return render.layout(content=content)
+
+class FamilyMemberCreate(object):
+	def POST(self):
+		form = web.input(btn_add=None, btn_reset=None, btn_cancel=None, name=None, age=None, gender=None, job=None, office=None)
+		if form.btn_add:
+			new_member_id = mongo_db_rft.family_members.count() + 1
+			mongo_db_rft.family_members.insert({"id" : new_member_id, 
+												"name" : form.name,
+												"age" : form.age,
+												"gender" : form.gender,
+												"job" : form.job,
+												"office" : form.office})
+			
+			session.selected_family_member = new_member_id
+			web.seeother("/family_members")
+			
+		elif form.btn_reset:
+			web.seeother("/family_member_add")
+		
+		elif form.btn_cancel:
+			web.seeother("/family_members")
+		
+		else:
+			web.seeother("/")	
+			
 if __name__ == "__main__":
 	app.run()
--- projects/rajesh_family_trust/tests/app_tests.py
+++ projects/rajesh_family_trust/tests/app_tests.py
@@ -17,6 +17,27 @@ def test_family_member():
 	assert_response(resp, contains="Rajesh Family Trust")
 	resp = app.request("/family_members")
 	assert_response(resp, contains="Family Members")
-	resp = app.request("/family_member", method="POST")
+	resp = app.request("/family_member")
 	assert_response(resp, contains="Family Member")
 	
+def test_family_member_edit():
+	resp = app.request("/")
+	assert_response(resp, contains="Rajesh Family Trust")
+	resp = app.request("/family_members")
+	assert_response(resp, contains="Family Members")
+	resp = app.request("/family_member")
+	assert_response(resp, contains="Family Member")
+	resp = app.request("/family_member_edit")
+	assert_response(resp, contains="Edit Family Member")
+
+def test_family_member_submit():
+	resp = app.request("/")
+	assert_response(resp, contains="Rajesh Family Trust")
+	resp = app.request("/family_members")
+	assert_response(resp, contains="Family Members")
+	resp = app.request("/family_member")
+	assert_response(resp, contains="Family Member")
+	resp = app.request("/family_member_edit")
+	assert_response(resp, contains="Edit Family Member")
+	resp = app.request("/family_member_submit", method="POST")
+	assert_response(resp, status="303")
